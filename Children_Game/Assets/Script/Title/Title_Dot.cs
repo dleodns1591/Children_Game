f8af23cb08d149a48910a83d78e5b6ea6cf6cdd9
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-05-31 20:36:19 +0900

    타이틀 완료
    타이틀 완료

--- Children_Game/Assets/Script/Title/Title_Dot.cs
+++ Children_Game/Assets/Script/Title/Title_Dot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class Title_Dot : MonoBehaviour
 {
@@ -12,13 +13,24 @@ public class Title_Dot : MonoBehaviour
     public float Earth_Speed;
 
     [Header("구름")]
-    public GameObject Clude_01;
-    public GameObject Clude_02;
-    public GameObject Clude_03;
-    public RectTransform Clude_04;
+    public RectTransform Cloud_01;
+    public RectTransform Cloud_02;
+    public RectTransform Cloud_03;
+    public RectTransform Cloud_04;
+
+    [Header("터치 후 게임 시작")]
+    public Image Touch_Start;
+    public float Start_Num = 0;
+
+    [Header("타이틀 & 부제목")]
+    public RectTransform Title;
+    public RectTransform SubTitle;
 
     void Start()
     {
+
+        Cloud_Dot();
+        TouchStart_Dot();
     }
 
     void Update()
@@ -31,17 +43,104 @@ public class Title_Dot : MonoBehaviour
         Earth.transform.Rotate(Vector3.up * Time.deltaTime * Earth_Speed);
     }
 
-    public void Clude_Dot()
+    public void Cloud_Dot()
     {
-        StartCoroutine(Clude());
+        StartCoroutine(Move_Cloud());
     }
 
-    public IEnumerator Clude()
+    public void TouchStart_Dot()
     {
-        Clude_04.DOAnchorPosX(690, 1f);
-        Clude_04.DOAnchorPosY(-412, 1f);
-        yield return new WaitForSeconds(1.5f);
-        Clude_04.DOAnchorPosX(640, 1f);
-        Clude_04.DOAnchorPosY(-489, 1f);
+        StartCoroutine(StartTxt_FadeIn());
     }
+
+    public void StartClick_Dot()
+    {
+        if (Start_Num == 0)
+        {
+            Start_Num += 1;
+            StopCoroutine(Move_Cloud());
+            StopCoroutine(ComeBack_Cloud());
+            StartCoroutine(Start_Clcick());
+        }
+
+    }
+
+    #region 구름 이동
+    public IEnumerator Move_Cloud()
+    {
+        if (Start_Num == 0)
+        {
+            Cloud_01.DOAnchorPosX(-872, 1f).SetEase(Ease.InOutQuad);
+            Cloud_01.DOAnchorPosY(293, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_02.DOAnchorPosX(865, 1f).SetEase(Ease.InOutQuad);
+            Cloud_02.DOAnchorPosY(375, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_03.DOAnchorPosX(-755, 1f).SetEase(Ease.InOutQuad);
+            Cloud_03.DOAnchorPosY(-472, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_04.DOAnchorPosX(690, 1f).SetEase(Ease.InOutQuad);
+            Cloud_04.DOAnchorPosY(-412, 1f).SetEase(Ease.InOutQuad);
+            yield return new WaitForSeconds(1.5f);
+            StartCoroutine(ComeBack_Cloud());
+        }
+    }
+
+    public IEnumerator ComeBack_Cloud()
+    {
+        if (Start_Num == 0)
+        {
+            Cloud_01.DOAnchorPosX(-761, 1f).SetEase(Ease.InOutQuad);
+            Cloud_01.DOAnchorPosY(453, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_02.DOAnchorPosX(727, 1f).SetEase(Ease.InOutQuad);
+            Cloud_02.DOAnchorPosY(481, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_03.DOAnchorPosX(-667, 1f).SetEase(Ease.InOutQuad);
+            Cloud_03.DOAnchorPosY(-519, 1f).SetEase(Ease.InOutQuad);
+
+            Cloud_04.DOAnchorPosX(640, 1f).SetEase(Ease.InOutQuad);
+            Cloud_04.DOAnchorPosY(-489, 1f).SetEase(Ease.InOutQuad);
+            yield return new WaitForSeconds(1.5f);
+            StartCoroutine(Move_Cloud());
+        }
+
+    }
+    #endregion
+
+    #region 게임시작 FadeInOut
+
+    public IEnumerator StartTxt_FadeIn()
+    {
+        Touch_Start.DOFade(0f, 1f);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(StartTxt_FadeOut());
+    }
+
+    public IEnumerator StartTxt_FadeOut()
+    {
+        if (Start_Num == 0)
+        {
+            Touch_Start.DOFade(1f, 1f);
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(StartTxt_FadeIn());
+        }
+    }
+
+    #endregion
+
+    #region 게임시작 클릭
+
+    public IEnumerator Start_Clcick()
+    {
+        Cloud_01.DOAnchorPosX(-1450, 1f).SetEase(Ease.InOutExpo);
+        Cloud_02.DOAnchorPosX(1537, 1f).SetEase(Ease.InOutExpo);
+        Cloud_03.DOAnchorPosX(-1450, 1f).SetEase(Ease.InOutExpo);
+        Cloud_04.DOAnchorPosX(1537, 1f).SetEase(Ease.InOutExpo);
+        yield return new WaitForSeconds(1f);
+        Title.DOAnchorPosY(700, 1f).SetEase(Ease.InOutBack);
+    }
+
+
+    #endregion
 }
